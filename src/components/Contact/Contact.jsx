0558964d08cf86a6168a0d070884c5ef60e3d15e
Manoj.A <manojamir2911@gmail.com>
2024-08-29 15:35:04 +0530

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,9 @@
-import {
-  FaRegEnvelope,
-  FaRegUser,
-  FaRegMap,
-  FaWhatsapp,
-} from "react-icons/fa";
+import { FaRegEnvelope, FaRegUser, FaRegMap, FaWhatsapp } from "react-icons/fa";
 import emailjs from "@emailjs/browser";
 import shapeOne from "../../assets/shape-1.png";
 import "./Contact.css";
 import { useRef, useState } from "react";
+import { message } from "antd";
 
 const Contact = () => {
   const [form, setForm] = useState({
@@ -19,22 +15,38 @@ const Contact = () => {
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
   const forms = useRef();
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    if (!form.name || form.name.length === 0) {
+      return message.error("Please enter Name to send message");
+    } if (!form.email || form.email.length === 0) {
+      return message.error("Please enter E-mail Id to send message");
+    }if(!validateEmail(form.email)){
+      return message.error("Please enter valid Email Id to send message");
+    }
+    if (!form.subject || form.subject.length === 0) {
+      return message.error("Please enter Subject to send message");
+    }
+    if (!form.message || form.message.length === 0) {
+      return message.error("Please enter Message to send message");
+    }
+   
     emailjs
-      .sendForm(
-        "service_tr19mud",
-        "template_lu7qrs7",
-        forms.current,
-        {
-          publicKey: "gfegF7SK4qkdqTwBc",
-        }
-      )
+      .sendForm("service_tr19mud", "template_lu7qrs7", forms.current, {
+        publicKey: "gfegF7SK4qkdqTwBc",
+      })
       .then(
         () => {
           console.log("SUCCESS!");
+          message.success("Message sent, Kindly Please check you Mail now.");
         },
         (error) => {
           console.log("FAILED...", error);
@@ -81,7 +93,7 @@ const Contact = () => {
 
           <div className="contact__card">
             <span className="contact__card-icon">
-            <FaWhatsapp />{" "}
+              <FaWhatsapp />{" "}
             </span>
             <div className="contact__card-title">Phone</div>
             <div className="contact__card-data">+91-9597388699</div>
@@ -110,7 +122,7 @@ const Contact = () => {
                 name="email"
                 value={form.email}
                 onChange={handleChange}
-                type="email"
+                type="text"
                 className="contact__form-input"
               />{" "}
             </div>{" "}
